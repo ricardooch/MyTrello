59a222fe35ac7d0fd23f6170c728fe9dd6d5af6b
Ricardo <ricardo8arr@gmail.com>
2023-06-25 00:13:40 -0600

    Draggable columns

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import Board from "@/components/Board";
 
 export default function Home() {
   return (
--- components/Board.tsx
+++ components/Board.tsx
@@ -1,9 +1,61 @@
-import { DragDropContext } from 'react-beautiful-dnd'
+'use client'
+import { useBoardStore } from '@/store/BoardStore'
+import { useEffect } from 'react'
+import { DragDropContext, Droppable } from 'react-beautiful-dnd'
+import Column from '@/components/Column'
 
 function Board() {
+  const [board, getBoard, setBoardState] = useBoardStore((state) => [
+    state.board,
+    state.getBoard,
+    state.setBoardState
+  ]);
+  
+
+  useEffect(() => {
+    getBoard();
+  }, [getBoard])
+
+  const handleOnDragEnd = (result: DropResult) => {
+    const { destination, source, type } = result;
+
+    // Check if user dragged card outside of board
+    if (!destination) return;
+
+    // Handle column drag
+    if (type === "column") {
+      const entries = Array.from(board.columns.entries());
+      const [removed] = entries.splice(source.index, 1); 
+      entries.splice(destination.index, 0, removed);
+      const rearrangedColumns = new Map(entries);
+      setBoardState({
+        ...board, columns: rearrangedColumns
+      })
+    }
+  };
+
   return (
-    <div>Board</div>
-  )
+  <DragDropContext onDragEnd={handleOnDragEnd}>
+    <Droppable droppableId="board" direction='horizontal' type='column'>
+        {(provided) => (
+            <div
+            className='grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto'
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >{
+              Array.from(board.columns.entries()).map(([id, column], index) => (
+                <Column
+                  key={id}
+                  id={id}
+                  todos={column.todos}
+                  index={index}
+                />
+              ))
+            }</div>
+        )}
+    </Droppable>
+  </DragDropContext>
+  );
 }
 
 export default Board
