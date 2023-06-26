40f5a4b14d3efa56c77dd75122b0acfd5ce6628c
Ricardo <ricardo8arr@gmail.com>
2023-06-25 22:32:52 -0600

    draggable todos

--- components/Board.tsx
+++ components/Board.tsx
@@ -5,10 +5,11 @@ import { DragDropContext, Droppable } from 'react-beautiful-dnd'
 import Column from '@/components/Column'
 
 function Board() {
-  const [board, getBoard, setBoardState] = useBoardStore((state) => [
+  const [board, getBoard, setBoardState, updateTodoInBD] = useBoardStore((state) => [
     state.board,
     state.getBoard,
-    state.setBoardState
+    state.setBoardState,
+    state.updateTodoInBD
   ]);
   
 
@@ -31,6 +32,63 @@ function Board() {
       setBoardState({
         ...board, columns: rearrangedColumns
       })
+    }
+
+    // This step is needed as the indexes are stoped as numbers 0,1,2 etc. instead of id's with DND library
+    const columns = Array.from(board.columns);
+    const startColIndex = columns[Number(source.droppableId)];
+    const finishColIndex = columns[Number(destination.droppableId)];
+
+    const startCol: Column = {
+      id: startColIndex[0],
+      todos: startColIndex[1].todos,
+    };
+
+    const finishCol: Column = {
+      id: finishColIndex[0],
+      todos: finishColIndex[1].todos,
+    };
+
+    if (!startCol || !finishCol) return;
+
+    if (source.index === destination.index && startCol === finishCol) return;
+
+    const newTodos = startCol.todos;
+    const [todoMoved] = newTodos.splice(source.index, 1)
+
+    if (startCol.id === finishCol.id) {
+      // Same column task drag
+      newTodos.splice(destination.index, 0, todoMoved);
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos,
+      };
+      const newColumns = new Map(board.columns);
+      newColumns.set(startCol.id, newCol);
+
+      setBoardState({ ...board, columns: newColumns });
+
+    } else {
+      // draggin to another column
+      const finishTodos = Array.from(finishCol.todos);
+      finishTodos.splice(destination.index, 0, todoMoved);
+
+      const newColumns = new Map(board.columns);
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos,
+      };
+
+      newColumns.set(startCol.id, newCol);
+      newColumns.set(finishCol.id, {
+        id: finishCol.id,
+        todos: finishTodos,
+      });
+
+      // Update in BD
+      updateTodoInBD(todoMoved, finishCol.id);
+
+      setBoardState({...board, columns: newColumns})
     }
   };
 
